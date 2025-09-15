d44e1d56b9a508187f20b18bf48d28c6e64812a5
ClearDev <tobeiokita35@gmail.com>
2025-09-15 17:26:36 +0700

    fix: database SSL configuration for production

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,12 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 import { env } from "@/data/env/server";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const db = drizzle({
-    schema,
-    connection: {
-        password: env.DB_PASSWORD,
-        user: env.DB_USER,
-        database: env.DB_NAME,
-        host: env.DB_HOST,
-    }
-})
+  schema,
+  connection: {
+    connectionString: env.POSTGRES_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
+  },
+});
